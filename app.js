1fb519fbeb5ebb24b156715d6dbbb089a28b0941
sumioka <sumioka.motoshi@jp.fujitsu.com>
2014-09-02 00:15:56 +0900

    group:proj, mobile. event: ball. web-api:connections

--- app.js
+++ app.js
@@ -30,24 +30,24 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.post('/', routes.index);
-//app.get('/users', user.list);
+
+// 接続数を表す
+var historical_proj = 0;
+var running_proj = 0;
+
+var historical_mobiles = 0;
+var running_mobiles = 0;
 
 
-var balls = {"id": "options"}; // {id1: options1, id2: options2, ...}
-
-app.get('/balls',function(req,res){
-	res.send(JSON.stringify(balls));
-});
+// 成績を計算する
 
 
 
+// server起動
 var server = http.createServer(app);
 server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
-
 
 
 // socket.io 利用
@@ -64,5 +64,56 @@ io.sockets.on('connection', function(socket){
 	socket.on('disconnect',function(){
 		console.log("disconnect");
 	});
+});
+
+
+// ビル
+var proj = io.of("/proj").on("connection", function(socket){
+	console.log("proj connection");
+	historical_proj ++;
+	running_proj++;
+	
+	// 切断
+	socket.on('disconnect',function(){
+		running_proj--;
+		console.log("proj disconnect");
+	});
+});
+
+// モバイル
+var mobile = io.of("/mobile").on("connection", function(socket){
+	console.log("mobile connection");
+	historical_mobiles ++;
+	running_mobiles++;
+	
+	// ボールに関するメッセージはビルへ
+	socket.on("ball", function(data){
+		proj.emit("ball", data);
+	});
+	
+	socket.on('disconnect',function(){
+		running_mobiles--;
+		console.log("mobile disconnect");
+	});
+	
+});
+
+
+
+
+
+// 接続数を見る
+app.get("/connections",function(req,res){
+	var connections = {
+		proj:{
+			running: running_proj,
+			historical: historical_proj
+		},
+		mobiles:{
+			running: running_mobiles,
+			historical: historical_mobiles
+		}
+	}
+	res.send(JSON.stringify(connections));
 });
 

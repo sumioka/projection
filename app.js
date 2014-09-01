2993eb4e172d750e93c448007dd6a91318bb74ca
sumioka <sumioka.motoshi@jp.fujitsu.com>
2014-09-02 01:22:50 +0900

    volatie test

--- app.js
+++ app.js
@@ -115,7 +115,8 @@ var mobile = io.of("/mobile").on("connection", function(socket){
 	
 	// ボールに関するメッセージはビルへ
 	socket.on("ball", function(data){
-		proj.volatile.emit("message",  {value: data.value});
+//		proj.volatile.emit("message",  {value: data.value});
+		proj.emit("message",  {value: data.value});
 	});
 	
 	// 正解は集計？そして、ビルへもメッセージ
